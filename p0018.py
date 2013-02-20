7b8a9b8bdab8c8abe59954d153c29e0d1ddde9cf
Praveen Kundurthy <p.kundurthy7@gmail.com>
2013-02-20 03:33:01 -0800

    after a while at p58' git push

--- p0018.py
+++ p0018.py
@@ -1,58 +1,27 @@
 
 import numpy as np
 
-
 fileObj = open('file.01.p0018.txt','r').readlines()
-
-DictTree = {}
+TList = []
 NLev = len(fileObj)
 
-for iL in range(NLev):
-	line = fileObj[iL]
-	maplist = map(int, line.split())
-	Nmap = len(maplist)
-	DictTree[iL] = {}
-	for iR in range(Nmap):
-		TR = iL-1,iR
-		TL = iL-1,iR-1
-		BR = iL+1,iR+1
-		BL = iL+1,iR
-		if iL == 0:
-			TR = None,None
-			TL = None,None
-		if iL == NLev-1:
-			BR = None,None
-			BL = None,None
-		if iR == 0:
-			TL = None,None
-		if iR == Nmap-1:
-			TR = None,None
- 
-		DictTree[iL][iR] = {'v':maplist[iR],\
-						    'tr':TR,'tl':TL,\
-							'br':BR,'bl':BL,\
-							'visited':False}
-
-iL = 0
-iR = 0
-maxSum = 0
-sumN = 0
-typeDef = 'bl'
-while True:
-	sumN += DictTree[iL][iR]['v']
-	DictTree[iL][iR]['visited'] = True
-	iL,iR = DictTree[iL][iR][typeDef] 
-	if iL == None and iR == None:
-		if sumN > maxSum:
-			maxSum = sumN
-		sumN -= DictTree[iL][iR]['v']
-		iL,iR = DictTree
-
-
-print sumN
-
-
-
+def maxArray(arrT,arrB):
 	
+	SumList = []
+	for j in range(len(arrT)):
+		sum1 = arrT[j] + arrB[j]
+		sum2 = arrT[j] + arrB[j+1]
+		SumList.append(max(sum1,sum2))
+	return SumList
 
+for i in range(NLev-2,-1,-1):
+	TList_T = map(int, fileObj[i].split())
+	TList_B = map(int, fileObj[i+1].split())
+
+	if i == NLev-2:
+		SumList = maxArray(TList_T,TList_B)
+	else:
+		SumList = maxArray(TList_T,SumList)
+
+print SumList
 
